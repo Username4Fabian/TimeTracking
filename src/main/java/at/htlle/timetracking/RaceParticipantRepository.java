068735a3b743d3715871d15c293d8ff71cf183dc
Username4Fabian <03anion.proxys@icloud.com>
2024-01-06 04:01:06 +0100

    Working Thumbnails

--- src/main/java/at/htlle/timetracking/RaceParticipantRepository.java
+++
@@ -1,6 +0,0 @@
-package at.htlle.timetracking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RaceParticipantRepository extends JpaRepository<RaceParticipant, Long> {
-}
